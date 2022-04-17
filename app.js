9218a590e971c7f36db30adf08d84b89f6d9e838
Mohd Ibrahim <raisashahg@gmail.com>
2022-04-17 08:50:02 +0530

    commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var seconds = 0;
+setInterval(() => {console.log(seconds);seconds+=1;}, 1000);
+var nodemailer = require("nodemailer");
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  pool: true,
+  auth: {
+    user: "raisashahg@gmail.com",
+    pass: "xfeltktrgimnoyqh",
+  },
+});
+var mailingList = [{
+  from: "raisashahg@gmail.com",
+  to: "irf44g@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "That was easy!",
+}];
+async function repeater() {
+  transporter.sendMail(mailingList[0], async function(error, info){
+    if (error) {
+    console.log(error);
+    } else {
+    console.log('Email sent: ' + info.response);
+    await reepeater();
+    }
+  });
+}
+async function reepeater() {
+  transporter.sendMail(mailingList[0], async function(error, info){
+    if (error) {
+    console.log(error);
+    } else {
+    console.log('Email sent: ' + info.response);
+    await repeater();
+    }
+  });
+}
+repeater();
